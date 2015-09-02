39174610510ce4218dea5f07ef0e22d15f7cac50
NORMA-Company <develop@norma.co.kr>
2015-09-02 10:01:42 +0900

    BugFix #33

--- module/airodump.py
+++ module/airodump.py
@@ -117,7 +117,7 @@ class Scanner(object):
             Popen(remove_command, shell=True, stdout=None, stderr=None)
         except OSError:
             pass
-        dump_command = ['airodump-ng', self.iface, '-w', self.PATH + 'atear']
+        dump_command = ['airodump-ng', self.iface, '-w', self.PATH + 'atear', '--channel', '1,2,3,4,5,6,7,8,9,10,11,12,13,36,40,44,48,52,56,60,64,100,104,108,112,116,120,124,128,132,136,140,144,149,153,161,165,169,38,46,54,62,102,110,118,126,134,142,151,157,159,42,58,106,122,138,155,50,114']
         dump_proc = Popen(dump_command, stdout=DN, stderr=DN)
         if self.START_SIGNAL:
             pass
